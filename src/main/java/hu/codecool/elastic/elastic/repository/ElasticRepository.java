2fed3ea672a92e69521649ee24e63fd7e7c7d690
tarapeti <gtarapeti@gmail.com>
2020-04-30 10:57:30 +0200

    find by id method added

--- src/main/java/hu/codecool/elastic/elastic/repository/ElasticRepository.java
+++ src/main/java/hu/codecool/elastic/elastic/repository/ElasticRepository.java
@@ -1,6 +1,7 @@
 package hu.codecool.elastic.elastic.repository;
 
 import hu.codecool.elastic.elastic.model.News;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
@@ -9,9 +10,13 @@ public interface ElasticRepository extends ElasticsearchRepository<News, String>
 
     List<News> findAll();
 
+    News findByIdLike(String id);
+
     List<News> findByContentContaining(String text);
 
     List<News> findByCreatedLike(String date);
+
+    void deleteAll();
 
     //List<News> findByContentContainingAndCreatedLike(String text, String date);
 }
